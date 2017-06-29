8737fcb20e25c8b156677e7a32f69cdcd4203c9c
David Chase <dchasepdx@gmail.com>
2017-06-29 15:42:41 -0700

    This commit locks in the multi column filtering. It works as long as you're adding filters. Once you start taking filters away it breaks.

--- src/MyTable.js
+++ src/MyTable.js
@@ -4,26 +4,81 @@ import data from './data.json';
 class MyTable extends Component {
   constructor(props) {
     super(props);
+    this.data = data.Players;
     this.state = {
-      data
+      players: this.data,
+      byes: {
+        ARI: 8, ATL: 5, BAL: 10,
+        BUF: 6, CAR: 11, CHI: 9,
+        CIN: 6, CLE: 9, DAL: 6,
+        DEN: 5, DET: 6, GB: 8,
+        HOU: 6, IND: 11, JAC: 8,
+        KC: 10, LAC: 9, LAR: 8,
+        MIA: 11, MIN: 9, NYG: 8,
+        NYJ: 11, NE: 9, NO: 5,
+        OAK: 10, PHI: 10, PIT: 9,
+        SF: 11, SEA: 6, TB: 11, 
+        TEN: 8, WAS: 5,
+      },
     };
+    this.filterFields = this.filterFields.bind(this);
+    this.resetFilters = this.resetFilters.bind(this);
+  }
+
+  resetFilters() {
+    this.setState({players: this.data});
+  }
+
+  filterFields(e, field) {
+    const {displayName, position, team, players} = this.state;
+    const filterData = e.target.value.toString().toLowerCase();
+    const list = (displayName || position || team) ? players : this.data;
+
+
+    const filteredList = list.filter(player => {
+      return player[field].toString().toLowerCase().indexOf(filterData) > -1;
+    });
+    this.setState({
+      players: filteredList,
+      [field]: filterData
+    });
   }
 
   render() {
-    const data = this.state.data.Players;
+    const players = this.state.players;
+    const byes = this.state.byes;
     return (
       <div className='table'>
-        <table>
+        <table>      
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Team</th>
-              <th>Position</th>
-              <th>Active</th>
+              <th>
+                <label htmlFor='name'>Name</label><br/>
+                <input 
+                  id='name'
+                  onChange={(e) => this.filterFields(e, 'displayName')}
+                />
+
+              </th>
+              <th>
+                <label htmlFor='Team'>Team</label><br/>
+                <input 
+                  id='Team'
+                  onChange={(e) => this.filterFields(e, 'team')}
+                />
+              </th>
+              <th>
+                <label htmlFor='Position'>Position</label><br/>
+                <input 
+                  id='Position'
+                  onChange={(e) => this.filterFields(e, 'position')}
+                />
+              </th>
+              <th>Bye</th>
             </tr>
           </thead>
           <tbody>
-            {data.map(player => {
+            {players.map(player => {
               if (parseInt(player.active, 10)) {
                 return (
                 <tr key={player.playerId}>
@@ -37,7 +92,7 @@ class MyTable extends Component {
                     {player.position}
                   </td>
                   <td>
-                    {player.active}
+                    {byes[player.team]}
                   </td>
                 </tr>
                 );
